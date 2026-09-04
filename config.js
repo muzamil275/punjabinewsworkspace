3f5051b4e0b7a2265f4f1670c74eced70bf2360c
Muhammad Muzamil <muhamadmuzamil275@gmail.com>
2026-09-04 16:56:22 +0500

    Centralize public app settings

--- config.js
+++ config.js
@@ -1,7 +1,10 @@
 window.PNW_CONFIG = {
-  API_URL: "/api",
-  SUPABASE_URL: "https://mqxoegglwznspxsjydjo.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_5zNYDsB3-esKhjLhewBCBA_z1pnkWHn",
-  OWNER_EMAIL: "muzamil.275pk@gmail.com",
-  OWNER_PHONE: "+923425078246"
+  API_URL: '/api',
+  SUPABASE_URL: 'https://mqxoegglwznspxsjydjo.supabase.co',
+  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_5zNYDsB3-esKhjLhewBCBA_z1pnkWHn',
+  OWNER_EMAIL: 'muzamil.275pk@gmail.com',
+  OWNER_PHONE: '+923425078246',
+  PREMIUM_PRICE: 500,
+  PAYMENT_EASYPAISA: '+92 342 5078246',
+  PAYMENT_UBL: 'PK91UNIL0109000315081244'
 };
