db7094a373e6abe8cd6875cd25654dac3a1f27f5
Muhammad Muzamil <muhamadmuzamil275@gmail.com>
2026-09-06 18:42:58 +0500

    Remove phone-based owner auth configuration

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ window.PNW_CONFIG = {
   SUPABASE_URL: 'https://mqxoegglwznspxsjydjo.supabase.co',
   SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_5zNYDsB3-esKhjLhewBCBA_z1pnkWHn',
   OWNER_EMAIL: 'muzamil.275pk@gmail.com',
-  OWNER_PHONE: '+923425078246',
   PREMIUM_PRICE: 500,
   PAYMENT_EASYPAISA: '+92 342 5078246',
   PAYMENT_UBL: 'PK91UNIL0109000315081244'
